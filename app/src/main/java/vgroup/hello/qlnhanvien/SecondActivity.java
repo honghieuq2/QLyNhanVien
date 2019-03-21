932e577637bb0ee1bd5b02d99d71cfe3b302e837
honghieuq2 <honghieuq2@gmail.com>
2019-03-21 11:54:46 +0700

    21/3/2019_3

--- app/src/main/java/vgroup/hello/qlnhanvien/SecondActivity.java
+++ app/src/main/java/vgroup/hello/qlnhanvien/SecondActivity.java
@@ -1,4 +1,16 @@
 package vgroup.hello.qlnhanvien;
 
-public class SecondActivity {
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+public class SecondActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+
+        Log.d("Haha", "Ahaha");
+    }
 }
